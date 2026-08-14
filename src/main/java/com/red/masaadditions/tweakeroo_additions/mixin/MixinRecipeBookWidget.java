3920356622cee07253fd6c7095cd912d76e7f114
FAMJAM1 <arsenijisakov496@gmail.com>
2026-08-14 15:26:53 +0800

    Take modifier keys from malilib and use the renamed container input
    Screen no longer exposes hasControlDown and friends, and slot clicks go
    through handleContainerInput.

--- src/main/java/com/red/masaadditions/tweakeroo_additions/mixin/MixinRecipeBookWidget.java
+++ src/main/java/com/red/masaadditions/tweakeroo_additions/mixin/MixinRecipeBookWidget.java
@@ -1,8 +1,8 @@
 package com.red.masaadditions.tweakeroo_additions.mixin;
 
 import com.red.masaadditions.tweakeroo_additions.config.ConfigsExtended;
+import fi.dy.masa.malilib.gui.GuiBase;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.recipebook.RecipeBookComponent;
 import net.minecraft.world.inventory.ContainerInput;
 import org.spongepowered.asm.mixin.Mixin;
@@ -19,8 +19,8 @@ public abstract class MixinRecipeBookWidget {
     // From UsefulMod by nessie
     @Inject(method = "refreshInputs", at = @At("RETURN"))
     private void refreshInputs(CallbackInfo ci) {
-        if (ConfigsExtended.Generic.CLICK_RECIPE_CRAFT.getBooleanValue() && Screen.hasControlDown() && Screen.hasShiftDown()) {
-            client.gameMode.clickSlot(client.player.containerMenu.containerId, 0, 1, Screen.hasAltDown() ? ContainerInput.THROW : ContainerInput.QUICK_MOVE, client.player);
+        if (ConfigsExtended.Generic.CLICK_RECIPE_CRAFT.getBooleanValue() && GuiBase.isCtrlDown() && GuiBase.isShiftDown()) {
+            client.gameMode.handleContainerInput(client.player.containerMenu.containerId, 0, 1, GuiBase.isAltDown() ? ContainerInput.THROW : ContainerInput.QUICK_MOVE, client.player);
         }
     }
 }
